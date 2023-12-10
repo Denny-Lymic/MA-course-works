b0d75b68c93572ed44b89adfb56b204cdfb5edb1
Denny-Lymic <denigma.silence@gmail.com>
2023-12-10 13:17:52 +0200

    Version 1

---
+++ Lab 4.3/Lab 4.3/Animals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4._3
+{
+    //6) implement interface IEnumerable
+    class Animals : IEnumerable<Animal>
+    {
+        // 7) declare private array of Animal
+
+        public Animal[] AnimalsList;
+
+        // 8) declare parameter constructor to initialize array   
+
+        public Animals(params Animal[] animals)
+        {
+            AnimalsList = animals;
+        }
+
+        // 9) implement method GetEnumerator(), which returns IEnumerator
+        // use foreach on array of Animal
+        // and yield return for every animal
+        public IEnumerator<Animal> GetEnumerator()
+        {
+            foreach(var animal in AnimalsList)
+            {
+                yield return animal; 
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return AnimalsList.GetEnumerator();
+        }
+    }
+}
